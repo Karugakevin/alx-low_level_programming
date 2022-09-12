b0047deb692eef8e64d4e80c193d648f8f75d02d
Setric muholo <muholosetric@gmail.com>
2022-09-12 22:39:42 +0000

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 0 to 99
+ *
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0 ; digit1 < 9 ; digit1++)
+	{
+		for (digit2 = 1 ; digit2 <= 9 ; digit2++)
+		{
+			if (digit1 > digit2)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+			}
+			if (digit1 != 8)
+				putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
