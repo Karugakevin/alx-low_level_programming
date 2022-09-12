a3211ee0aea6754383fa007b425f55ef81bbfdaa
Setric muholo <muholosetric@gmail.com>
2022-09-12 23:31:53 +0000

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,36 +7,23 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	int i;
-	int j;
-	int k;
-	int m;
+	int num1, num2;
 
-	for (i = 48 ; i < 58 ; i++)
+	for (num1 = 0 ; num1 <= 98 ; num1++)
 	{
-		for (j = 48 ; j < 58 ; j++)
+		for (num2 = num1 + 1 ; num2 <= 99 ; num2++)
 		{
-			for (k = 48 ; k < 58 ; k++)
-			{
-				for (m = 48 ; m < 58 ; m++)
-				{
-				a = (i * 10) + j;
-			b = (k * 10) + m;
-		if (b > a)
-		{
-		putchar(i);
-	putchar(j);
-putchar(' ');
-putchar(m);
-putchar(k);
-if (i == 57 && j == 58 && k == 57 && i == 57)
-	putchar(',');
-putchar(' ');
-		}
-				}
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
