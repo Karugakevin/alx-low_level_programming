223067f3b539ff131f374619d452f43fa91f059b
Setric muholo <muholosetric@gmail.com>
2022-09-14 16:30:43 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		for (num = i ; num < 10 ; num++)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
