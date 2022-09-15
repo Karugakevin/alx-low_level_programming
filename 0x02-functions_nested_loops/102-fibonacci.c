8de5e83bad954e4d49f663c607f33339cb803265
Setric muholo <muholosetric@gmail.com>
2022-09-15 16:13:44 +0000

    Task 13

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int i;
-	unsigned long fib1, fib2, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (i = 1 ; i <= 50 ; ++i)
+	for (i = 0 ; i < 50 ; i++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
@@ -19,7 +19,7 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 
-		if (i == 50)
+		if (i == 49)
 			printf("\n");
 		else
 			printf(", ");
