f8d7ab544c56b43d407392805cd775b11163f648
Setric muholo <muholosetric@gmail.com>
2022-09-15 22:42:35 +0000

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,22 +10,20 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n >= 1)
 	{
 		_putchar('+');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(0);
-		_putchar('\n');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar('\n');
 		return (-1);
 	}
+	_putchar('\n');
 }
