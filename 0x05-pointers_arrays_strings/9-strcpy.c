fcb424fae7a1ce746803f4fe401c2a13907806fb
Setric muholo <muholosetric@gmail.com>
2022-09-19 20:28:51 +0000

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src,
+ * @dest: Array
+ * @src: value of array
+ * Return: it returns debt
+ *
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	_putchar('\n');
+	return (dest);
+}
