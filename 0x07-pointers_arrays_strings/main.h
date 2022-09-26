eaeb0c8d526c20cb3d1124c017bef1c6b16f1a16
Setric muholo <muholosetric@gmail.com>
2022-09-26 21:51:16 +0000

    Task 6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
 #endif /*MAIN_H*/
