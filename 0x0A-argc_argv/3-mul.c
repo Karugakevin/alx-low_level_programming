a865e0ccc8653324d011cb24c3ce2f5bfa43713d
Setric muholo <muholosetric@gmail.com>
2022-09-30 18:12:08 +0000

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, n, mul;
+	int i,  mul = 1;
 
-	if (argc != 3)
+	if (argc < 3 || argc >= 4)
 	{
 		printf("Error\n");
-		return (1);
+		return(1);
 	}
-	
-	i = atoi(argv[1]);
-	n = atoi(argv[2]);
-	mul = i * n;
-	
-	printf("%d\n", mul);
+	else
+	{
+		for (i = 1; i < argc; ++i)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
 
 	return (0);
 }
