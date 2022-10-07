8246c785ee0c605cdc4032cc9723c46f36db6e33
Setric muholo <muholosetric@gmail.com>
2022-10-07 02:32:42 +0000

    Task 3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,4 +5,5 @@ void *malloc_checked(unsigned int b);
 int _putchar(char c);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
 #endif
