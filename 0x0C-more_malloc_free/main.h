74ecf699a558029134562d63c8c2f72937bdadfe
Setric muholo <muholosetric@gmail.com>
2022-10-07 01:34:16 +0000

    Task 4

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,4 +4,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
 void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
