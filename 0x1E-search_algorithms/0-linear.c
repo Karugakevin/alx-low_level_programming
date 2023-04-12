49264c28e4fd36dc555c273b4c665c08d8d7dfbe
Setric muholo <muholosetric@gmail.com>
2023-04-12 12:27:37 +0000

    fixed error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (index = 0; index < size; index++)
 	{
-		printf("Value of checked array[%ld] = [ %d]\n", index,  array[index]);
+		printf("Value checked array[%ld] = [ %d]\n", index,  array[index]);
 
 		if (array[index] == value)
 		{
